4258967c003697ee21b3883f31532c4e946e86c5
pchoksi <parv.choksi81@outlook.com>
2024-02-21 16:18:38 -0700

    Added dotenv to hide password and username and added .gitignore file.

--- js/nodejs/database.js
+++ js/nodejs/database.js
@@ -1,19 +1,28 @@
+
+
+// This is just for testing purposes when I run it in terminal to know the script is starting.
+console.log("Starting the database connection script...");
+
+// Error tracking, figuring out what errors are happening while connecting to the database
 process.on('uncaughtException', (err) => {
     console.log('There was an uncaught error', err);
     process.exit(1);
 });
 
-console.log("Starting the database connection script...");
-
+// Require these two (dotenv so user/password can't be seen on github)
+require('dotenv').config();
 const mysql = require('mysql');
 
+// Creating connection with the details
 const connection = mysql.createConnection({
-    host : "test",
-    user : "test",
-    password : "test",
-    database : "test"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
+
+// Error messages again
 connection.connect(err => {
     if(err) {
         console.error("Error connecting " + err.stack);
@@ -23,5 +32,6 @@ connection.connect(err => {
     console.log("Connected as ID " + connection.threadId);
 });
 
+// End connection
 connection.end();
 
