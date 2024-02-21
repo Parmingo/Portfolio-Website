b073e09b2b8e48ae5c677a43c281a43d21cb4dc8
pchoksi <parv.choksi81@outlook.com>
2024-02-20 17:34:51 -0700

    Added typewriter script. Now going to work on the portfolio page.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+
+/**
+ * This is a type writer script to type out the name behind the cursor. Adjusting the speed will change how fast it is typed (0 is fast and 100 is slow).
+ */
+var i = 0;
+var txt = "Parv Choksi";
+var speed = 90;
+
+function typeWriter() {
+    if(i < txt.length) {
+        document.querySelector(".homepage-name").innerHTML = txt.substring(0, i + 1) + '<span> |</span>';
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+window.onload = typeWriter;
