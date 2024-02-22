cc32f0a30be65e3b7ebd31ca37fb0b3a1035d798
pchoksi <parv.choksi81@outlook.com>
2024-02-21 19:54:48 -0700

    Established connection to database

--- js/nodejs/database.js
+++ js/nodejs/database.js
@@ -1,37 +1,25 @@
 
 
-// This is just for testing purposes when I run it in terminal to know the script is starting.
-console.log("Starting the database connection script...");
-
-// Error tracking, figuring out what errors are happening while connecting to the database
-process.on('uncaughtException', (err) => {
-    console.log('There was an uncaught error', err);
-    process.exit(1);
-});
-
-// Require these two (dotenv so user/password can't be seen on github)
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const fs = require('fs');
 
-// Creating connection with the details
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl  : {
+        ca : fs.readFileSync(process.env.DB_SSL)
+      }
 });
 
-
-// Error messages again
 connection.connect(err => {
-    if(err) {
-        console.error("Error connecting " + err.stack);
+    if (err) {
+        console.error("Error connecting: " + err.stack);
         return;
     }
-
-    console.log("Connected as ID " + connection.threadId);
+    console.log("Connected to PlanetScale!");
 });
 
-// End connection
 connection.end();
-
